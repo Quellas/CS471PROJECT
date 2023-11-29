fe9a010674cddc62d3d7974164ef64c3f0d5e0f9
Quellas <lqx.1017@163.com>
2023-11-28 22:22:23 -0500

    ProducerConsumer update

--- PRODUCERCONSUMER/ProducerConsumer.java
+++ PRODUCERCONSUMER/ProducerConsumer.java
@@ -1,6 +1,8 @@
 package PRODUCERCONSUMER;
 
 import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
 
 public class ProducerConsumer {
@@ -16,105 +18,92 @@ public class ProducerConsumer {
 
     private static volatile boolean stopThreads = false;
 
-    public static void main(String[] args) {
+    private static  int sleepTime = 2000;
+    private static int producerThreads = 16;
+    private static int consumerThreads = 4;
+    private static CountDownLatch producerLatch = new CountDownLatch(producerThreads);
+    private static CountDownLatch consumerLatch = new CountDownLatch(consumerThreads);
 
-        long startTime = System.currentTimeMillis();
 
-        args = new String[] { "2000", "1", "16" };
-        if (args.length != 3) {
-            System.out.println("Must pass three positive integer parameters");
-            return;
-        }
+    public static void main(String[] args) throws InterruptedException {
 
-        int sleepTime = 2000;
-        int producerThreads = 16;
-        int consumerThreads = 4;
 
-        if (sleepTime < 0 || producerThreads < 0 || consumerThreads < 0) {
-            System.out.println("Must pass three positive integer parameters");
-            return;
-        }
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter sleep time in milliseconds: ");
+         sleepTime = sc.nextInt();
+         System.out.println("Enter number of producer threads: ");
+         producerThreads = sc.nextInt();
+         System.out.println("Enter number of consumer threads: ");
+         consumerThreads = sc.nextInt();
 
-        // Create the producer threads and insert into array
-        Thread[] producerThreadHandles = new Thread[producerThreads];
-        for (int i = 0; i < producerThreads; i++) {
-            Thread producerThread = new Thread(Producer);
-            producerThreadHandles[i] = producerThread;
-            producerThread.start();
-        }
+         long startTime = System.currentTimeMillis();
+         if (sleepTime < 0 || producerThreads < 0 || consumerThreads < 0) {
+             System.out.println("Must pass three positive integer parameters");
+             return;
+         }
 
-        // Create the consumer threads and insert into array
-        Thread[] consumerThreadHandles = new Thread[consumerThreads];
-        for (int i = 0; i < consumerThreads; i++) {
-            Thread consumerThread = new Thread(Consumer);
-            consumerThreadHandles[i] = consumerThread;
-            consumerThread.start();
-        }
 
-        // Sleep and allow the program to run
-        try {
-            Thread.sleep(sleepTime);
-            // Signal threads to stop
-            stopThreads = true;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+         // Create the producer threads and insert into array
+         Thread[] producerThreadHandles = new Thread[producerThreads];
+         for (int i = 0; i < producerThreads; i++) {
+             Thread producerThread = new Thread(Producer);
+             producerThreadHandles[i] = producerThread;
+             producerThread.start();
 
-     int count1=0;
-        // Join on threads
-        for (Thread producerThread : producerThreadHandles) {
-            try {
-                producerThread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            count1++;
-            if(count1==producerThreads){
-                break;
+         }
 
-            }
-        }
+         // Create the consumer threads and insert into array
+         Thread[] consumerThreadHandles = new Thread[consumerThreads];
+         for (int i = 0; i < consumerThreads; i++) {
+             Thread consumerThread = new Thread(Consumer);
+             consumerThreadHandles[i] = consumerThread;
+             consumerThread.start();
 
-        int count2=0;
-        for (Thread consumerThread : consumerThreadHandles) {
-            try {
-                consumerThread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            if(count2==consumerThreads){
-                break;
 
-            }
-        }
+         }
 
-        long endTime = System.currentTimeMillis();
-        long turnaroundTime = endTime - startTime;
+         // Sleep and allow the program to run
+         try {
+             Thread.sleep(sleepTime);
+             // Signal threads to stop
+             stopThreads = true;
+         } catch (InterruptedException e) {
+             e.printStackTrace();
+         }
 
-        System.out.println("Overall turnaround time: " + turnaroundTime + " milliseconds");
+         long turnaroundTime = 0l;
+         long endTime = System.currentTimeMillis();
+         turnaroundTime = endTime - startTime;
+         System.out.println("Overall turnaround time: " + turnaroundTime + " milliseconds");
+
+         return;
+
+
     }
 
-    private static Runnable Producer = () -> {
-        while (!stopThreads) {
+     static Runnable Producer = () -> {
+        while (!Thread.interrupted()) {
             try {
                 Thread.sleep(2000);
                 Random random = new Random();
                 int randomProduced = random.nextInt();
+               //int item = insertItem(randomProduced);
 
                 if (insertItem(randomProduced) != 0) {
                     System.out.println("Could not insert item");
                 }
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                //e.printStackTrace();
             }
             if(stopThreads){
                 break;
             }
         }
+        producerLatch.countDown();
     };
 
     private static Runnable Consumer = () -> {
-        while (!stopThreads) {
+        while (!Thread.interrupted()) {
             try {
                 Thread.sleep(2000);
                 //Random random = new Random();
@@ -123,15 +112,16 @@ public class ProducerConsumer {
                     System.out.println("Could not remove item");
                 }
             } catch (InterruptedException e) {
-                e.printStackTrace();
+               // e.printStackTrace();
             }
             if(stopThreads){
                 break;
             }
         }
+        consumerLatch.countDown();
     };
 
-    private static int insertItem(int item) throws InterruptedException {
+    static int insertItem(int item) throws InterruptedException {
         semFull.acquire();
         mutex.acquire();
 
@@ -139,7 +129,6 @@ public class ProducerConsumer {
         count++;
         System.out.println("Producer produced " + item + " count = " + count);
         buffer[insertPosition] = item;
-       // insertPosition = (insertPosition + 1) % BUFFER_SIZE;
         ++insertPosition;
         if (insertPosition >=5) {
             insertPosition = 0;
@@ -155,7 +144,8 @@ public class ProducerConsumer {
         return 0;
     }
 
-    private static int removeItem(int[] item) throws InterruptedException {
+
+     static int removeItem(int[] item) throws InterruptedException {
         semEmpty.acquire();
         mutex.acquire();
 
@@ -163,7 +153,6 @@ public class ProducerConsumer {
         count--;
         item[0] = buffer[removePosition];
         System.out.println("Consumer consumed " + item[0] + " count = " + count);
-        //removePosition = (removePosition + 1) % BUFFER_SIZE;
          ++removePosition;
          if (removePosition >=5) {
             removePosition = 0;
