ac453f8e881e42635dafe280f743611294587ae6
Quellas <lqx.1017@163.com>
2023-11-28 23:07:30 -0500

    ProducerConsumer final touches

--- PRODUCERCONSUMER/ProducerConsumer.java
+++ PRODUCERCONSUMER/ProducerConsumer.java
@@ -1,8 +1,6 @@
 package PRODUCERCONSUMER;
-
 import java.util.Random;
 import java.util.Scanner;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
 
 public class ProducerConsumer {
@@ -11,22 +9,14 @@ public class ProducerConsumer {
     private static Semaphore mutex = new Semaphore(1);
     private static Semaphore semEmpty = new Semaphore(0);
     private static Semaphore semFull = new Semaphore(BUFFER_SIZE);
-
     private static int insertPosition = 0;
     private static int removePosition = 0;
     private static int count = 0;
-
-    private static volatile boolean stopThreads = false;
-
-    private static  int sleepTime = 2000;
-    private static int producerThreads = 16;
-    private static int consumerThreads = 4;
-    private static CountDownLatch producerLatch = new CountDownLatch(producerThreads);
-    private static CountDownLatch consumerLatch = new CountDownLatch(consumerThreads);
-
+    private static  int sleepTime = 0;
+    private static int producerThreads = 0;
+    private static int consumerThreads = 0;
 
     public static void main(String[] args) throws InterruptedException {
-
 
          Scanner sc = new Scanner(System.in);
          System.out.println("Enter sleep time in milliseconds: ");
@@ -38,35 +28,23 @@ public class ProducerConsumer {
 
          long startTime = System.currentTimeMillis();
          if (sleepTime < 0 || producerThreads < 0 || consumerThreads < 0) {
-             System.out.println("Must pass three positive integer parameters");
+             System.out.println("Please enter 3 positive integers");
              return;
          }
 
-
-         // Create the producer threads and insert into array
-         Thread[] producerThreadHandles = new Thread[producerThreads];
          for (int i = 0; i < producerThreads; i++) {
              Thread producerThread = new Thread(Producer);
-             producerThreadHandles[i] = producerThread;
              producerThread.start();
-
          }
 
-         // Create the consumer threads and insert into array
-         Thread[] consumerThreadHandles = new Thread[consumerThreads];
          for (int i = 0; i < consumerThreads; i++) {
              Thread consumerThread = new Thread(Consumer);
-             consumerThreadHandles[i] = consumerThread;
              consumerThread.start();
-
-
          }
 
          // Sleep and allow the program to run
          try {
              Thread.sleep(sleepTime);
-             // Signal threads to stop
-             stopThreads = true;
          } catch (InterruptedException e) {
              e.printStackTrace();
          }
@@ -76,71 +54,57 @@ public class ProducerConsumer {
          turnaroundTime = endTime - startTime;
          System.out.println("Overall turnaround time: " + turnaroundTime + " milliseconds");
 
-         return;
-
-
+        System.exit(0);
     }
 
      static Runnable Producer = () -> {
-        while (!Thread.interrupted()) {
+        while (true) {
             try {
-                Thread.sleep(2000);
+                Thread.sleep(1000);
                 Random random = new Random();
                 int randomProduced = random.nextInt();
-               //int item = insertItem(randomProduced);
-
                 if (insertItem(randomProduced) != 0) {
-                    System.out.println("Could not insert item");
+                    System.out.println("Fail to insert item");
                 }
             } catch (InterruptedException e) {
-                //e.printStackTrace();
+                e.printStackTrace();
             }
-            if(stopThreads){
-                break;
-            }
+
         }
-        producerLatch.countDown();
     };
 
     private static Runnable Consumer = () -> {
-        while (!Thread.interrupted()) {
+        while (true) {
             try {
-                Thread.sleep(2000);
-                //Random random = new Random();
+                Thread.sleep(1000);
                 int[] randomNum = new int[20];
                 if (removeItem(randomNum) != 0) {
-                    System.out.println("Could not remove item");
+                    System.out.println("Fail to remove item");
                 }
             } catch (InterruptedException e) {
-               // e.printStackTrace();
+               e.printStackTrace();
             }
-            if(stopThreads){
-                break;
-            }
+
         }
-        consumerLatch.countDown();
     };
 
     static int insertItem(int item) throws InterruptedException {
         semFull.acquire();
         mutex.acquire();
-
         // Insert the item
         count++;
-        System.out.println("Producer produced " + item + " count = " + count);
+        System.out.println("Producer produced item: " + item + " count: " + count);
         buffer[insertPosition] = item;
         ++insertPosition;
         if (insertPosition >=5) {
             insertPosition = 0;
         }else if (insertPosition<0||count>5) {
-            System.out.println("Error in removing item");
+            System.out.println("Error in inserting item");
             return -1;
         }
-
         // Release the mutex and semEmpty to allow remove to run
         mutex.release();
         semEmpty.release();
-
         return 0;
     }
 
@@ -148,11 +112,10 @@ public class ProducerConsumer {
      static int removeItem(int[] item) throws InterruptedException {
         semEmpty.acquire();
         mutex.acquire();
-
         // Remove the item
         count--;
         item[0] = buffer[removePosition];
-        System.out.println("Consumer consumed " + item[0] + " count = " + count);
+        System.out.println("Consumer consumed item: " + item[0] + " count: " + count);
          ++removePosition;
          if (removePosition >=5) {
             removePosition = 0;
@@ -163,7 +126,6 @@ public class ProducerConsumer {
         // Release the mutex and semFull to allow insert to run
         mutex.release();
         semFull.release();
-
         return 0;
     }
 }
